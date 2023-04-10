c61a40a59bd665088f263b0a0ac996ec92b5bacf
Ljuzifer <121060706+Ljuzifer@users.noreply.github.com>
2023-04-11 02:20:16 +0300

    Images in live page from GitHub + js in main.js

---
+++ src/main.js
@@ -0,0 +1,9 @@
+// Modal popups
+
+import './js/mobile';
+import './js/sub-modal';
+import './js/modal-review';
+import './js/modal-buynow';
+import './js/swiper-ourproducts';
+import './js/swiper-review';
+// import 'https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js';
