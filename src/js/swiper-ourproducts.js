83535b29e8636d58ca0c9397e18cca3d6f2de3ea
MariaPanchenko <marysa2502@gmail.com>
2023-04-10 16:11:40 +0300

    feature_swiper-slider
    add swiper.js in index.html, attached swiper in main-products.html and add css swiper styles

---
+++ src/js/swiper-ourproducts.js
@@ -0,0 +1,30 @@
+var swiper = new Swiper('.swiper', {
+  // slidesPerView: 1,
+  // spaceBetween: 10,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 18,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 18,
+    },
+    // when window width is >= 640px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 18,
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 18,
+    },
+  },
+});
